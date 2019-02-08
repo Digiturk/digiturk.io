699fda0cd18f11a54f7bdfa0aa50a0c99a3f010a
MEHMET BARAN (TECHNOLOGY) <mehmetbaran@mehmetbaran.net>
2019-02-08 13:35:42 +0300

    minor fixes

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -11,65 +11,64 @@ module.exports = {
         filename: "[name].bundle.js",
         chunkFilename: "[name].chunk.js"
     },
-
     resolve: {
-        extensions: [".js", ".jsx", ".ts", ".tsx", ".md"]
-    },
+            extensions: [".js", ".jsx", ".ts", ".tsx", ".md"]
+        },
 
-    module: {    
-        rules: [
-            {            
-                test: /(^.?|\.[^d]|[^.]d|[^.][^d])\.ts$/,
-                // include: path.join(__dirname, "src"),
-                loader: "ts-loader",
-                options: {
-                    transpileOnly: true // IMPORTANT! use transpileOnly mode to speed-up compilation
-                }
-            },
-            {
-                test: /\.tsx$/,
-                // include: path.join(__dirname, "src"),
-                loader: "ts-loader",
-                options: {
-                    transpileOnly: true // IMPORTANT! use transpileOnly mode to speed-up compilation
-                }
-            },
-            {
-                test: /\.(png|jpg|gif|ico|html)$/,
-                loader: 'file-loader'
-            },            
-            {
-                test: /.(LICENSE|md|lock|.d.ts)$/,
-                loader: "ignore-loader"
-            }, 
-            {
-                test: /\.mdx$/,
-                use: [
-                  {
+        module: {
+            rules: [
+                {
+                    test: /(^.?|\.[^d]|[^.]d|[^.][^d])\.ts$/,
+                    // include: path.join(__dirname, "src"),
+                    loader: "ts-loader",
+                    options: {
+                        transpileOnly: true // IMPORTANT! use transpileOnly mode to speed-up compilation
+                    }
+                },
+                {
+                    test: /\.tsx$/,
+                    // include: path.join(__dirname, "src"),
+                    loader: "ts-loader",
+                    options: {
+                        transpileOnly: true // IMPORTANT! use transpileOnly mode to speed-up compilation
+                    }
+                },
+                {
+                    test: /\.(png|jpg|gif|ico|html)$/,
+                    loader: 'file-loader'
+                },
+                {
+                    test: /.(LICENSE|md|lock|.d.ts)$/,
+                    loader: "ignore-loader"
+                },
+                {
+                    test: /\.mdx$/,
+                    use: [
+                        {
+                            loader: 'babel-loader',
+                            options: {
+                                presets: ['react']
+                            }
+                        },
+                        {
+                            loader: '@mdx-js/loader'
+                        }
+                    ]
+                },
+                {
+                    test: /\.jsx$/,
                     loader: 'babel-loader',
                     options: {
-                      presets: ['react']
+                        presets: ['react']
                     }
-                  }, 
-                  {
-                    loader: '@mdx-js/loader'
-                  }
-                ]
-            },
-            {
-                test: /\.jsx$/,
-                loader: 'babel-loader',
-                options: {
-                    presets: ['react']
                 }
-            }
-        ]
-    },
+            ]
+        },
 
-    devtool: "source-map",
-    devServer: {
-        contentBase: "./dist",
-        historyApiFallback: true,
-        port:2020
-    }
-};
+        devtool: "source-map",
+        devServer: {
+            contentBase: "./dist",
+            historyApiFallback: true,
+            port: 2020
+        }
+    };
